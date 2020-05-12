cd1003dffa685bce71e5674d1f4fa2c258e183f3
jemikalajah123 <philip.jemikalajah@konga.local>
2020-05-12 17:14:38 +0100

    Validation and sanitising of application inputs is completed

--- ContactList.php
+++ ContactList.php
@@ -10,10 +10,12 @@ class ContactList{
         $this->contact_list[] = $contact;
   
     }
+    
     public function listContact(){
 
         return $this->contact_list;
     }
+
     public function removeContact($email){
 
         foreach($this->contact_list as $key => $value) {
@@ -23,11 +25,13 @@ class ContactList{
         }
         
     }
+
     public function searchContact($name){
         foreach($this->contact_list as $items) {
-            if ($items->name == $name){
+            if ($items->name === $name){
                 $this->searched_contact[] = $items;
             }
+            return false;
     
         }
   
